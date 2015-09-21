2b8e714345b0079cb8ba63c186907f5edab61da1
YoungHun Kim <yh8004.kim@samsung.com>
2015-09-21 19:25:48 +0900

    Update the mmsvc with muse including module_t and remove the handle argument when dispatching
    Change-Id: I15b9e88505ab9547063c8b93e5564af9b0585c12

--- include/muse_core_config.h
+++ include/muse_core_config.h
@@ -1,5 +1,5 @@
 /*
- * mmsvc-core
+ * muse-core
  *
  * Copyright (c) 2000 - 2011 Samsung Electronics Co., Ltd. All rights reserved.
  *
@@ -19,8 +19,8 @@
  *
  */
 
-#ifndef __MMSVC_CORE_CONFIG_H__
-#define __MMSVC_CORE_CONFIG_H__
+#ifndef __MUSE_CORE_CONFIG_H__
+#define __MUSE_CORE_CONFIG_H__
 
 #ifdef _cplusplus
 extern "C" {
@@ -28,13 +28,13 @@ extern "C" {
 
 #include <iniparser.h>
 
-#define CONFFILE "/usr/share/mused/mused.conf"
+#define CONFFILE "/usr/share/muse/muse.conf"
 #define HOST_MAX_COUNT 1024
-#define MUSED_MAX_PARAM_NUM 10
-#define MUSED_MAX_PARAM_STRLEN 256
-#define MUSEDHOST "mused:hosts"
-#define MUSEDLOG "mused:logfile"
-#define MUSEDGST "mused:gstparam"
+#define MUSE_MAX_PARAM_NUM 10
+#define MUSE_MAX_PARAM_STRLEN 256
+#define MUSEHOST "muse:hosts"
+#define MUSELOG "muse:logfile"
+#define MUSEGST "muse:gstparam"
 #define COLON ":"
 #define COMMA ","
 #define PATH "path"
@@ -44,27 +44,27 @@ typedef struct host_info
 	char *path;
 } host_info_t;
 
-typedef struct config
+typedef struct muse_core_config
 {
 	char *hosts;
 	int type;
 	char *logfile;
-	char *gst_param_str[MUSED_MAX_PARAM_NUM];
+	char *gst_param_str[MUSE_MAX_PARAM_NUM];
 	int gst_param_cnt;
 	host_info_t *host_infos[HOST_MAX_COUNT];
-	dictionary *mmsvc_dict;
+	dictionary *muse_dict;
 	void (*free)(void);
 	char* (*get_path)(int);
 	int (*get_gst_param_cnt)(void);
 	char* (*get_gst_param_str)(int);
 	char* (*get_hosts)(void);
-} config_t;
+} muse_core_config_t;
 
-/*mmsvc_core_config_init must be called before mmsvc_core_config_get_instance*/
-config_t *mmsvc_core_config_get_instance(void);
-void mmsvc_core_config_init(void);
+/*muse_core_config_init must be called before muse_core_config_get_instance*/
+muse_core_config_t *muse_core_config_get_instance(void);
+void muse_core_config_init(void);
 
 #ifdef __cplusplus
 }
 #endif
-#endif /* __MMSVC_CORE_CONFIG_H__ */
+#endif /* __MUSE_CORE_CONFIG_H__ */
